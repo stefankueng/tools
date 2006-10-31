fb18981f5374126963a80408535c0a594eac245b
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2006-10-31 18:41:55 +0000

    initial import

---
+++ src/SRBand.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <shlobj.h>
+
+#include "Globals.h"
+#include "ResizableGrip.h"
+#include <set>
+#include <map>
+
+#define DB_CLASS_NAME (TEXT("SRBarClass"))
+
+#define MIN_SIZE_X   100
+#define MIN_SIZE_Y   20
+
+#define IDM_COMMAND  0
+#define ID_SELECTTIMER 101
+
+#define BUTTONSIZEX	60
+#define SPACEBETWEENEDITANDBUTTON 0
+
+#define NUMINTERNALCOMMANDS 4
+/**
+ * Desk Band.
+ * Implements a desk band used by the shell.
+ * This requires the implementation of several interfaces.
+ */
+class CDeskBand : public IDeskBand 
+	, public IInputObject
+	, public IObjectWithSite
+	, public IPersistStream
+{
+protected:
+	DWORD m_ObjRefCount;
+
+public:
+	CDeskBand();
+	~CDeskBand();
+
+	// IUnknown methods
+	STDMETHODIMP QueryInterface(REFIID, LPVOID*);
+	STDMETHODIMP_(DWORD) AddRef();
+	STDMETHODIMP_(DWORD) Release();
+
+	// IOleWindow methods
+	STDMETHOD (GetWindow) (HWND*);
+	STDMETHOD (ContextSensitiveHelp) (BOOL);
+
+	// IDockingWindow methods
+	STDMETHOD (ShowDW) (BOOL fShow);
+	STDMETHOD (CloseDW) (DWORD dwReserved);
+	STDMETHOD (ResizeBorderDW) (LPCRECT prcBorder, IUnknown* punkToolbarSite, BOOL fReserved);
+
+	// IDeskBand methods
+	STDMETHOD (GetBandInfo) (DWORD, DWORD, DESKBANDINFO*);
+
+	// IInputObject methods
+	STDMETHOD (UIActivateIO) (BOOL, LPMSG);
+	STDMETHOD (HasFocusIO) (void);
+	STDMETHOD (TranslateAcceleratorIO) (LPMSG);
+
+	// IObjectWithSite methods
+	STDMETHOD (SetSite) (IUnknown*);
+	STDMETHOD (GetSite) (REFIID, LPVOID*);
+
+	// IPersistStream methods
+	STDMETHOD (GetClassID) (LPCLSID);
+	STDMETHOD (IsDirty) (void);
+	STDMETHOD (Load) (LPSTREAM);
+	STDMETHOD (Save) (LPSTREAM, BOOL);
+	STDMETHOD (GetSizeMax) (ULARGE_INTEGER*);
+
+	HWND GetParent(){return m_hwndParent;}
+private:
+	BOOL			m_bFocus;			///< true if the deskband or one of its children has the focus
+	HWND			m_hwndParent;		///< handle of the parent window, usually the shell
+	HWND			m_hWnd;				///< window handle of the deskband itself
+	HWND			m_hWndToolbar;		///< window handle of the toolbar which hold the button
+	HWND			m_hWndEdit;			///< window handle of the edit control
+	DWORD			m_dwViewMode;		///< viewing mode of the deskband
+	DWORD			m_dwBandID;			///< the ID of the deskband
+	IInputObjectSite *m_pSite;			///< the IInputObjectSite of the parent
+	WNDPROC			m_oldEditWndProc;	///< pointer to the original window proc of the edit control
+	HIMAGELIST		m_hToolbarImgList;	///< image list of the toolbar
+	SIZE			m_tbSize;			///< the max size of the toolbar
+
+	std::set<std::wstring> m_filelist;	///< list of files in the current view
+	std::wstring	m_filterstring;		///< the filter string
+	CResizableGrip	m_grip;				///< the grip used in the rename dialog
+	bool			m_bHideUnchanged;	///< true if the file which don't get renamed should be hidden
+
+	static std::map<DWORD, CDeskBand*> m_desklist;	///< map of thread-ID's and CDeskBand objects which use the keyboard hook
+	HHOOK			m_hook;				///< handle of the keyboard hook procedure
+private:
+	/// window procedure of the sub classed edit control
+	static LRESULT CALLBACK	EditProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam);
+	/// rename dialog callback function
+	static INT_PTR CALLBACK	RenameDlgFunc(HWND, UINT, WPARAM, LPARAM);
+
+	static LRESULT CALLBACK KeyboardHookProc(int code, WPARAM wParam, LPARAM lParam);
+
+	/// change the focus
+	void					FocusChange(BOOL);
+	/// called when the deskband looses focus
+	LRESULT					OnKillFocus(void);
+	/// called when the deskband gains focus
+	LRESULT					OnSetFocus(void);
+	/// window procedure of the deskband
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT uMessage, WPARAM wParam, LPARAM lParam);
+	/// handles WM_COMMAND messages
+	LRESULT					OnCommand(WPARAM wParam, LPARAM lParam);
+	/// handles WM_SIZE messages
+	LRESULT					OnSize(LPARAM);
+	/// handles WM_MOVE messages
+	LRESULT					OnMove(LPARAM);
+	/// registers and creates the deskband and the child controls
+	BOOL					RegisterAndCreateWindow(void);
+	/// set up the toolbar
+	BOOL					BuildToolbarButtons();
+};
+
---
+++ src/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by StExBar.rc
+//
+#define IDI_BTNICON                     101
+#define IDI_RENDLG                      103
+#define IDI_CMD                         104
+#define IDI_COPYNAME                    105
+#define IDI_COPYPATH                    106
+#define IDI_ICON2                       107
+#define IDI_OPTIONS                     107
+#define IDC_LIST1                       1001
+#define IDC_EDIT2                       1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        108
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
