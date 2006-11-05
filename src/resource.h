25791a90ccdeb347d4b17a10801cceed448eb138
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2006-11-05 16:48:26 +0000

    Add new command to rename the selected entries.

--- src/resource.h
+++ src/resource.h
@@ -11,20 +11,27 @@
 #define IDI_OPTIONS                     107
 #define IDD_OPTIONS                     108
 #define IDI_DEFAULT                     109
-#define IDI_ICON1                       110
 #define IDI_NEWFOLDER                   110
+#define IDD_RENAMEDLG                   111
+#define IDI_ICON1                       112
+#define IDI_RENAME                      112
 #define IDC_LIST1                       1001
+#define IDC_FILELIST                    1001
 #define IDC_EDIT2                       1003
+#define IDC_REPLACESTRING               1003
 #define IDC_SHOWTEXT                    1007
 #define IDC_EDITCONFIG                  1009
+#define IDC_MATCHSTRING                 1010
+#define IDC_MATCHLABEL                  1011
+#define IDC_REPLACELABEL                1012
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_RESOURCE_VALUE        113
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_CONTROL_VALUE         1013
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
