d938aaccd938a4e78978f87cd2c24ec99c1fa2db
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2006-11-05 08:59:11 +0000

    Add new command: New Folder This command creates a new folder in the current folder view using the standard windows way to do it: via the context menu of the folder background. After creating the new folder, the new item is put into edit mode like the user is used to.

--- src/resource.h
+++ src/resource.h
@@ -10,8 +10,9 @@
 #define IDI_ICON2                       107
 #define IDI_OPTIONS                     107
 #define IDD_OPTIONS                     108
-#define IDI_ICON1                       109
 #define IDI_DEFAULT                     109
+#define IDI_ICON1                       110
+#define IDI_NEWFOLDER                   110
 #define IDC_LIST1                       1001
 #define IDC_EDIT2                       1003
 #define IDC_SHOWTEXT                    1007
@@ -21,7 +22,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           101
