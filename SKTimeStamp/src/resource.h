8b774b3887e5dea29a5c4155b91d80ce6d132512
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2008-05-29 19:38:48 +0000

    Add date/time controls for the creation-, last-accessed- and last-modified-dates of the selected files. Only the first file is used for now (that will be improved later). Also, the date/times can't yet be modified.

--- SKTimeStamp/src/resource.h
+++ SKTimeStamp/src/resource.h
@@ -4,8 +4,14 @@
 //
 #define IDD_PROPPAGE_LARGE              107
 #define IDD_PROPPAGE                    107
-#define IDC_BUTTON1                     1001
 #define IDC_TOUCH                       1001
+#define IDC_DATECREATED                 1002
+#define IDC_TIMECREATED                 1003
+#define IDC_DATEMODIFIED                1004
+#define IDC_TIMEMODIFIED                1005
+#define IDC_DATEACCESSED                1006
+#define IDC_TIMECREATED3                1007
+#define IDC_TIMEACCESSED                1007
 
 // Next default values for new objects
 // 
@@ -13,7 +19,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
