501e4a2939138642e213a534c37a52e080e761cf
steveking <steveking@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2008-05-29 18:03:48 +0000

    * Fix property page title * add first button (no functions behind it yet)

--- trunk/SKTimeStamp/src/resource.h
+++ trunk/SKTimeStamp/src/resource.h
@@ -4,6 +4,8 @@
 //
 #define IDD_PROPPAGE_LARGE              107
 #define IDD_PROPPAGE                    107
+#define IDC_BUTTON1                     1001
+#define IDC_TOUCH                       1001
 
 // Next default values for new objects
 // 
@@ -11,7 +13,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        101
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
