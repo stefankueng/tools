c43a4cdfdb063289604a25d30216d30db8f43b2d
steveking <steveking@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2008-08-02 09:29:52 +0000

    Tag the 1.5.1 release of the StExBar

---
+++ tags/StExBar-1.5.1/src/Guid.h
@@ -0,0 +1,31 @@
+// StExBar - an explorer toolbar
+
+// Copyright (C) 2007-2008 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#pragma once
+
+// the GUIDs used in this COM-Object
+
+#ifdef _WIN64
+// {6C7A85A7-27C6-49ce-98B2-A8479B0DD63D}
+DEFINE_GUID(CLSID_StExBand, 
+			0x6c7a85a7, 0x27c6, 0x49ce, 0x98, 0xb2, 0xa8, 0x47, 0x9b, 0xd, 0xd6, 0x3d);
+#else
+// {367D8B32-F9FD-474b-8E65-9E521F35DE99}
+DEFINE_GUID(CLSID_StExBand, 
+			0x367d8b32, 0xf9fd, 0x474b, 0x8e, 0x65, 0x9e, 0x52, 0x1f, 0x35, 0xde, 0x99);
+#endif
---
+++ tags/StExBar-1.5.1/src/InfoDlg.cpp
@@ -0,0 +1,80 @@
+// StExBar - an explorer toolbar
+
+// Copyright (C) 2008 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "InfoDlg.h"
+
+#include <mshtmhst.h>
+
+#pragma comment(lib, "Urlmon.lib")
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CInfoDlg::CInfoDlg()
+{
+
+}
+
+CInfoDlg::~CInfoDlg()
+{
+
+}
+
+//Function which takes input of An HTML Resource Id
+BOOL CInfoDlg::ShowDialog(UINT idAboutHTMLID, HINSTANCE hInstance)
+{
+	//Load the IE Specific MSTML Interface DKK
+	HINSTANCE   hinstMSHTML = LoadLibrary(TEXT("MSHTML.DLL"));
+	BOOL bSuccess = FALSE;
+	if(hinstMSHTML)
+	{
+		SHOWHTMLDIALOGFN  *pfnShowHTMLDialog;
+		//Locate The Function ShowHTMLDialog in the Loaded MSHTML.DLL 
+		pfnShowHTMLDialog =     (SHOWHTMLDIALOGFN*)GetProcAddress(hinstMSHTML, "ShowHTMLDialog");
+		if(pfnShowHTMLDialog)
+		{
+			LPTSTR lpszModule = new TCHAR[_MAX_PATH];
+			//Get The Application Path
+			if (GetModuleFileName(hInstance, lpszModule, _MAX_PATH))
+			{
+				//Add the IE Res protocol
+				TCHAR strResourceURL[MAX_PATH*4];
+				_stprintf_s(strResourceURL, MAX_PATH*4, _T("res://%s/%d"), lpszModule, idAboutHTMLID);
+				size_t iLength = _tcslen(strResourceURL);
+			    LPWSTR lpWideCharStr = NULL;
+				lpWideCharStr =  new wchar_t[iLength+1];
+				//Attempt to Create the URL Moniker to the specified in the URL String
+				IMoniker *pmk;
+				if(SUCCEEDED(CreateURLMoniker(NULL,strResourceURL,&pmk)))
+				{
+					//Invoke the ShowHTMLDialog function by pointer
+					//passing the HWND of your Application , the Moniker,
+					//the remaining parameters can be set to NULL
+					pfnShowHTMLDialog(NULL,pmk,NULL,NULL,NULL);
+					bSuccess = TRUE;
+				}
+				delete lpWideCharStr;
+			}
+			delete [] lpszModule;
+		}
+		FreeLibrary(hinstMSHTML);
+	}
+	return bSuccess;
+}
