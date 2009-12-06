0ef7f3a947d075855dea4cfae6a1b20103ea7ece
steveking <steveking@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2009-12-06 11:37:47 +0000

    * move the tabspace project to the misc folder * add quick and dirty tool to switch on/off the monitor remotely

---
+++ trunk/Misc/MonitorRemote/MonitorClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MonitorClient
+{
+    class MonitorClient
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Out.Write("specify an ip address to connect to and ON or OFF as the second parameter");
+                return;
+            }
+            if (args.Length == 1)
+            {
+                Console.Out.Write("specify ON or OFF as the second parameter");
+                return;
+            }
+            IPAddress ipadr = IPAddress.Parse(args[0]);
+            IPEndPoint localEndPoint = new IPEndPoint(ipadr, 8000);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            newsock.Connect(localEndPoint);
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+            newsock.Send(encoding.GetBytes("Monitor" + args[1]));
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
