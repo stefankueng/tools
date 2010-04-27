44fc90c2681739aa69f1e37d524556e93d049f59
steveking <steveking@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2010-04-27 18:41:19 +0000

    First import of a small tool to adjust the screen brightness from the system tray.

---
+++ trunk/Misc/SKBright/src/MainWindow.h
@@ -0,0 +1,63 @@
+// SKBright - tool to adjust the monitor brightness
+
+// Copyright (C) 2010 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#pragma once
+#include "basewindow.h"
+#include "Resource.h"
+#include "shellapi.h"
+#include "shlwapi.h"
+#include <commctrl.h>
+
+
+
+class CMainWindow : public CWindow
+{
+public:
+    CMainWindow(HINSTANCE hInst, const WNDCLASSEX* wcx = NULL)
+        : CWindow(hInst, wcx)
+        , hwndNextViewer(NULL)
+        , foregroundWND(NULL)
+    {
+        SetWindowTitle((LPCTSTR)ResString(hResource, IDS_APP_TITLE));
+    };
+
+    ~CMainWindow(void)
+    {
+    };
+
+    bool                RegisterAndCreateWindow();
+
+protected:
+    /// the message handler for this window
+    LRESULT CALLBACK    WinMsgHandler(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    LRESULT             HandleCustomMessages(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    /// Handles all the WM_COMMAND window messages (e.g. menu commands)
+    LRESULT             DoCommand(int id);
+
+    void                ShowTrayIcon();
+    DWORD               GetDllVersion(LPCTSTR lpszDllName);
+
+protected:
+    NOTIFYICONDATA      niData;
+    HWND                hwndNextViewer;
+    HWND                foregroundWND;
+
+    typedef BOOL(__stdcall *PFNCHANGEWINDOWMESSAGEFILTER)(UINT message, DWORD dwFlag);
+    static PFNCHANGEWINDOWMESSAGEFILTER m_pChangeWindowMessageFilter;
+};
