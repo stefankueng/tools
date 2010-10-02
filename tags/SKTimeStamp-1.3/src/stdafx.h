a13282a0eb68b2dc15b824677030c502c33fb915
steveking <steveking@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2010-10-02 14:09:49 +0000

    Tag the 1.3 release of SKTimeStamp.

---
+++ tags/SKTimeStamp-1.3/src/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define ISOLATION_AWARE_ENABLED 1
+
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+#include <commctrl.h>
+#include <ShellApi.h>
+#include <Shlobj.h>
+#include <Shlwapi.h>
+#include <tchar.h>
+
+
