cb307c24a429045c73259b89963b558ceb6eeb73
steveking <steveking@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2011-04-09 16:32:33 +0000

    Set the common control V6 manifest.

--- trunk/Misc/AAClr/src/stdafx.h
+++ trunk/Misc/AAClr/src/stdafx.h
@@ -20,3 +20,6 @@
 
 
 #pragma comment(lib, "gdiplus.lib")
+
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
