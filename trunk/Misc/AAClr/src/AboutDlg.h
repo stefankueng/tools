5aea72ce7f378fce5aa385472ac395d309d12a17
XhmikosR <XhmikosR@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2012-06-13 07:36:15 +0000

    AAClr: remove an unneeded using namespace std and replace another one by explicitly using std

--- trunk/Misc/AAClr/src/AboutDlg.h
+++ trunk/Misc/AAClr/src/AboutDlg.h
@@ -22,8 +22,6 @@
 #include "hyperlink.h"
 #include <string>
 
-using namespace std;
-
 /**
  * bookmarks dialog.
  */
@@ -34,9 +32,9 @@ public:
     ~CAboutDlg(void);
 
 protected:
-    LRESULT CALLBACK        DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
-    LRESULT                 DoCommand(int id, int msg);
+    LRESULT CALLBACK    DlgFunc(HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lParam);
+    LRESULT             DoCommand(int id, int msg);
 private:
-    HWND                    m_hParent;
-    CHyperLink              m_link;
+    HWND                m_hParent;
+    CHyperLink          m_link;
 };
