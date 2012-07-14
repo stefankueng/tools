db561ba3509cd4f7d094d7f54ecc6d5f94b276bd
XhmikosR <xhmikosr@gmail.com>
2012-07-14 02:28:41 +0000

    tabspace.rc: use the unicode symbol for copyright stdafx.cpp: add a license header for consistency

--- Misc/tabspace/src/stdafx.cpp
+++ Misc/tabspace/src/stdafx.cpp
@@ -1,8 +1,20 @@
-// stdafx.cpp : source file that includes just the standard includes
-// tabspace.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
+// tabspace - converts tabs to spaces and vice-versa in multiple files
+
+// Copyright (C) 2011-2012 - Stefan Kueng
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
 
 #include "stdafx.h"
-
-// TODO: reference any additional headers you need in stdafx.h
-// and not in this file
