7fac7b9b2e771b735e500b1cd75ec21c400ad2cc
steveking <steveking@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2012-08-25 08:23:13 +0000

    (Closes issue #187) : tabspace: allow exluding files and/or masks

--- trunk/Misc/tabspace/src/tabspace.cpp
+++ trunk/Misc/tabspace/src/tabspace.cpp
@@ -1,6 +1,6 @@
 // tabspace - converts tabs to spaces and vice-versa in multiple files
 
-// Copyright (C) 2011 - Stefan Kueng
+// Copyright (C) 2011-2012 - Stefan Kueng
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -29,15 +29,66 @@
 
 
 std::set<std::wstring> g_allowedPatterns;
+std::set<std::wstring> g_excludedPatterns;
+
+int wcswildcmp(const wchar_t *wild, const wchar_t *string)
+{
+    const wchar_t *cp = NULL;
+    const wchar_t *mp = NULL;
+    while ((*string) && (*wild != '*'))
+    {
+        if ((*wild != *string) && (*wild != '?'))
+        {
+            return 0;
+        }
+        wild++;
+        string++;
+    }
+    while (*string)
+    {
+        if (*wild == '*')
+        {
+            if (!*++wild)
+            {
+                return 1;
+            }
+            mp = wild;
+            cp = string+1;
+        }
+        else if ((*wild == *string) || (*wild == '?'))
+        {
+            wild++;
+            string++;
+        }
+        else
+        {
+            wild = mp;
+            string = cp++;
+        }
+    }
+
+    while (*wild == '*')
+    {
+        wild++;
+    }
+    return !*wild;
+}
+
 
 bool FileExtensionInPattern(const std::wstring& filepath)
 {
-    const TCHAR * pFound = _tcsrchr(filepath.c_str(), '.');
+    std::wstring lowercasepath = filepath;
+    std::transform(lowercasepath.begin(), lowercasepath.end(), lowercasepath.begin(), std::tolower);
+
+    for (auto it = g_excludedPatterns.cbegin(); it != g_excludedPatterns.cend(); ++it)
+    {
+        if (wcswildcmp(it->c_str(), lowercasepath.c_str()))
+            return false;
+    }
+    const TCHAR * pFound = _tcsrchr(lowercasepath.c_str(), '.');
     std::wstring ext;
     if (pFound)
         ext = pFound + 1;
-
-    std::transform(ext.begin(), ext.end(), ext.begin(), std::tolower);
 
     return (g_allowedPatterns.find(ext) != g_allowedPatterns.end());
 }
@@ -51,24 +102,26 @@ int _tmain(int argc, _TCHAR* argv[])
     bool    bUseSpaces              =   true;
     int     tabsize                 =   4;
     bool    bRemoveEOLWhitespaces   =   true;
-    std::wstring filepattern        =   _T("c;cpp;cxx;cc;h;hpp;hxx;cs");
+    std::wstring filepattern        =   L"c;cpp;cxx;cc;h;hpp;hxx;cs";
 
 
     LPWSTR lpCmdLine = GetCommandLine();
     CCmdLineParser parser(lpCmdLine);
 
-    if (parser.HasKey(_T("?")) || parser.HasKey(_T("help")))
+    if (parser.HasKey(L"?") || parser.HasKey(L"help"))
     {
         // print help text and get out
-        _fputts(_T("usage:\n"), stdout);
-        _fputts(_T("tabspace [/path:\"path\\to\\check\"] [/checkonly] [/usetabs] [/tabsize:4] [/leaveeol] [/ext:\"ext;ension;list\"\n"), stdout);
-        _fputts(_T("/path      : the path to scan, defaults to the current directory\n"), stdout);
-        _fputts(_T("/checkonly : if specified, the files are not modified but only an info is shown\n"), stdout);
-        _fputts(_T("/usetabs   : convert spaces to tabs instead of tabs to spaces\n"), stdout);
-        _fputts(_T("/tabsize   : specifies the tab size, defaults to 4\n"), stdout);
-        _fputts(_T("/leaveol   : if specified, whitespaces at the end of lines are not removed\n"), stdout);
-        _fputts(_T("/ext       : a list of file extensions to scan, other extensions are ignored.\n"), stdout);
-        _fputts(_T("             defaults to \"c;cpp;cxx;cc;h;hpp;hxx;cs\"\n"), stdout);
+        _fputts(L"usage:\n", stdout);
+        _fputts(L"tabspace [/path:\"path\\to\\check\"] [/checkonly] [/usetabs] [/tabsize:4] [/leaveeol] [/ext:\"ext;ension;list\"\n", stdout);
+        _fputts(L"/path      : the path to scan, defaults to the current directory\n", stdout);
+        _fputts(L"/checkonly : if specified, the files are not modified but only an info is shown\n", stdout);
+        _fputts(L"/usetabs   : convert spaces to tabs instead of tabs to spaces\n", stdout);
+        _fputts(L"/tabsize   : specifies the tab size, defaults to 4\n", stdout);
+        _fputts(L"/leaveol   : if specified, whitespaces at the end of lines are not removed\n", stdout);
+        _fputts(L"/ext       : a list of file extensions to scan, other extensions are ignored.\n", stdout);
+        _fputts(L"             defaults to \"c;cpp;cxx;cc;h;hpp;hxx;cs\"\n", stdout);
+        _fputts(L"/exclude   : a list of patterns to ignore, separated by ';'\n", stdout);
+        _fputts(L"             for example \"c:\\sub1\\*.*;*\\sub2\\*.cpp\"\n", stdout);
         return 0;
     }
 
@@ -76,31 +129,31 @@ int _tmain(int argc, _TCHAR* argv[])
     TCHAR cwd[MAX_PATH] = {0};
     GetCurrentDirectory(MAX_PATH, cwd);
 
-    if (parser.HasVal(_T("path")))
+    if (parser.HasVal(L"path"))
     {
-        _tcscpy_s(cwd, MAX_PATH, parser.GetVal(_T("path")));
+        _tcscpy_s(cwd, MAX_PATH, parser.GetVal(L"path"));
     }
-    if (parser.HasVal(_T("ext")))
+    if (parser.HasVal(L"ext"))
     {
-        filepattern = parser.GetVal(_T("ext"));
+        filepattern = parser.GetVal(L"ext");
     }
-    if (parser.HasKey(_T("checkonly")))
+    if (parser.HasKey(L"checkonly"))
         bCheckOnly = true;
-    if (parser.HasKey(_T("usetabs")))
+    if (parser.HasKey(L"usetabs"))
         bUseSpaces = false;
-    if (parser.HasVal(_T("tabsize")))
-        tabsize = parser.GetLongVal(_T("tabsize"));
-    if (parser.HasKey(_T("leaveeol")))
+    if (parser.HasVal(L"tabsize"))
+        tabsize = parser.GetLongVal(L"tabsize");
+    if (parser.HasKey(L"leaveeol"))
         bRemoveEOLWhitespaces = false;
 
     // split the file extension string into the extensions
     {
         g_allowedPatterns.clear();
         // skip delimiters at beginning.
-        std::string::size_type lastPos = filepattern.find_first_not_of(_T(";"), 0);
+        std::string::size_type lastPos = filepattern.find_first_not_of(L";", 0);
 
         // find first "non-delimiter".
-        std::string::size_type pos = filepattern.find_first_of(_T(";"), lastPos);
+        std::string::size_type pos = filepattern.find_first_of(L";", lastPos);
 
         while (std::string::npos != pos || std::string::npos != lastPos)
         {
@@ -110,10 +163,37 @@ int _tmain(int argc, _TCHAR* argv[])
             g_allowedPatterns.insert(ext);
 
             // skip delimiters. Note the "not_of"
-            lastPos = filepattern.find_first_not_of(_T(";"), pos);
+            lastPos = filepattern.find_first_not_of(L";", pos);
 
             // find next "non-delimiter"
-            pos = filepattern.find_first_of(_T(";"), lastPos);
+            pos = filepattern.find_first_of(L";", lastPos);
+        }
+    }
+    // split the file extension string into the extensions
+    {
+        if (parser.HasVal(L"exclude"))
+        {
+            filepattern = parser.GetVal(L"exclude");
+        }
+        g_excludedPatterns.clear();
+        // skip delimiters at beginning.
+        std::string::size_type lastPos = filepattern.find_first_not_of(L";", 0);
+
+        // find first "non-delimiter".
+        std::string::size_type pos = filepattern.find_first_of(L";", lastPos);
+
+        while (std::string::npos != pos || std::string::npos != lastPos)
+        {
+            // found a token, add it to the set.
+            std::wstring ext = filepattern.substr(lastPos, pos - lastPos);
+            std::transform(ext.begin(), ext.end(), ext.begin(), std::tolower);
+            g_excludedPatterns.insert(ext);
+
+            // skip delimiters. Note the "not_of"
+            lastPos = filepattern.find_first_not_of(L";", pos);
+
+            // find next "non-delimiter"
+            pos = filepattern.find_first_of(L";", lastPos);
         }
     }
 
@@ -147,9 +227,9 @@ int _tmain(int argc, _TCHAR* argv[])
             }
             else
             {
-                _fputts(_T("error: could not load file '"), stderr);
+                _fputts(L"error: could not load file '", stderr);
                 _fputts(filepath.c_str(), stderr);
-                _fputts(_T("'\n"), stderr);
+                _fputts(L"'\n", stderr);
             }
         }
     }
