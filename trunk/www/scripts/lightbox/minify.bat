0d24010458e533b66373871bd728e46c75c91cd2
XhmikosR <XhmikosR@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2012-11-02 16:34:00 +0000

    minify style.css too

--- trunk/www/scripts/lightbox/minify.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem install node.js and then run "npm install -g uglify-js"
-
-set PATH=C:\MSYS\bin;%PATH%
-
-pushd %~dp0
-
-cat builder.js effects.js scriptaculous.js lightbox.js | uglifyjs -o pack.js -nc
