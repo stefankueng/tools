7f92e27346b1d82d0ed62fd6e5e2973674d79d9d
XhmikosR <XhmikosR@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2012-11-02 16:41:12 +0000

    use spaces

--- trunk/StExBar/contrib/attrib.js
+++ trunk/StExBar/contrib/attrib.js
@@ -10,6 +10,7 @@ var objArgs,num;
 
 objArgs = WScript.Arguments;
 num = objArgs.length;
+
 if (num < 1)
 {
     WScript.Echo("Usage: [CScript | WScript] attrib.js path/to/file");
@@ -23,18 +24,20 @@ var filesstring = objArgs(0).replace(/\"(.*)\"/, "$1");
 var files = filesstring.split("*");
 var fileindex=0;
 var errormsg = "";
+
 while (fileindex < files.length)
 {
-	var f = files[fileindex];
-	fso = fs.GetFile(f);
-	if (fso.attributes & 1)
-	{
-		fso.attributes = fso.attributes - 1;
-	}
-	else
-	{
-		fso.attributes = fso.attributes + 1;
-	}	
+    var f = files[fileindex];
+    fso = fs.GetFile(f);
+    if (fso.attributes & 1)
+    {
+        fso.attributes = fso.attributes - 1;
+    }
+    else
+    {
+        fso.attributes = fso.attributes + 1;
+    }
     fileindex+=1;
 }
+
 WScript.Quit(0);
