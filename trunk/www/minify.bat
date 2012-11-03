e20657538ead991a8166c7aebd1048eefd234c37
XhmikosR <XhmikosR@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2012-11-03 09:08:42 +0000

    minify.bat: use type since we are on Windows

--- trunk/www/minify.bat
+++ trunk/www/minify.bat
@@ -3,9 +3,7 @@ rem install node.js and then run:
 rem npm install -g clean-css
 rem npm install -g uglify-js
 
-set PATH=C:\MSYS\bin;%PATH%
-
 pushd %~dp0
 
-cat css/lightbox.css css/style.css | cleancss -o css/pack.css && ^
-uglifyjs --no-copyright -o js/lightbox.min.js js/lightbox.js
+type css\lightbox.css css\style.css | cleancss -o css\pack.css && ^
+uglifyjs --no-copyright -o js\lightbox.min.js js\lightbox.js
