78b520cfbc02c884b3172b08772348b8b840d02f
XhmikosR <xhmikosr@gmail.com>
2012-11-04 13:20:48 +0000

    add normalize.css, trimmed down for our needs

--- www/minify.bat
+++ www/minify.bat
@@ -5,5 +5,5 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
-type css\style.css css\lightbox.css | cleancss -o css\pack.css && ^
+type css\style.css css\lightbox.css css\normalize.css | cleancss --s0 -o css\pack.css && ^
 uglifyjs --no-copyright -o js\lightbox.min.js js\lightbox.js
