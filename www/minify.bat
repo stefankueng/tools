f6eea9123b65ab5f34bfc373fe224197cce66ba7
XhmikosR <xhmikosr@gmail.com>
2012-11-06 13:52:09 +0000

    compress header.png more and change the order we minify css

--- www/minify.bat
+++ www/minify.bat
@@ -5,5 +5,5 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
-type css\jquery.fancybox.css css\style.css css\normalize.css | cleancss --s0 -o css\pack.css
+type css\style.css css\normalize.css css\jquery.fancybox.css | cleancss --s0 -o css\pack.css
 type js\jquery.fancybox.js js\jquery.mousewheel.js | uglifyjs --no-copyright -o js\pack.js
