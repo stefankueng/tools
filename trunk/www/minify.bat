bbdad01918df4229a786979edfde2e12ca4a1b93
XhmikosR <XhmikosR@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2012-11-21 16:36:55 +0000

    switch to uglifyjs 2.2.0

--- trunk/www/minify.bat
+++ trunk/www/minify.bat
@@ -6,6 +6,6 @@ rem npm install -g uglify-js
 pushd %~dp0
 
 type css\style.css css\normalize.css css\jquery.fancybox.css | cleancss --s0 -o css\pack.css
-type js\jquery.fancybox.js js\jquery.mousewheel.js | uglifyjs --no-copyright -o js\pack.js
+uglifyjs js\jquery.fancybox.js js\jquery.mousewheel.js --compress --mangle -o js\pack.js
 
 popd
