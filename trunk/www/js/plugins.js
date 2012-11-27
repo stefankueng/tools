9d4de48899cc46c9c178134de484ad0f1bbe65f4
XhmikosR <XhmikosR@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2012-11-27 09:46:13 +0000

    * add a scroll to top option based on jQuery * move google analytics code in plugins.js which is taken from html5 boilerplate * rename menuText and menuFile in generatesite.py.tmpl to header* * remove unneeded anchor links from h1 tags

---
+++ trunk/www/js/plugins.js
@@ -0,0 +1,62 @@
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true,
+  strict:true, undef:true, unused:true, curly:true, browser:true, jquery:true,
+  trailing:true*/
+
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    "use strict";
+    var method;
+    var noop = function noop() {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+
+$(document).ready(function() {
+    "use strict";
+    $(".fancybox").fancybox({});
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+    });
+
+    $('.scrollup').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 'normal'); /*slow, normal, fast, 600 (ms)*/
+        return false;
+    });
+
+});
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-716525-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    "use strict";
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
