56f7a36e8f89b73a4135472fef7ade7f671a4791
XhmikosR <xhmikosr@gmail.com>
2012-11-27 09:46:13 +0000

    * add a scroll to top option based on jQuery * move google analytics code in plugins.js which is taken from html5 boilerplate * rename menuText and menuFile in generatesite.py.tmpl to header* * remove unneeded anchor links from h1 tags

--- www/minify.bat
+++ www/minify.bat
@@ -6,6 +6,6 @@ rem npm install -g uglify-js
 pushd %~dp0
 
 type css\style.css css\normalize.css css\jquery.fancybox.css | cleancss --s0 -o css\pack.css
-cmd /c uglifyjs js\jquery.fancybox.js js\jquery.mousewheel.js --compress --mangle -o js\pack.js
+cmd /c uglifyjs js\plugins.js js\jquery.fancybox.js js\jquery.mousewheel.js --compress --mangle -o js\pack.js
 
 popd
