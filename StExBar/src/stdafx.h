315cf804d0b6c07cb4305cecb02f97b363be068f
XhmikosR <xhmikosr@gmail.com>
2012-11-29 07:24:25 +0000

    bump _WIN32_IE to 0x700

--- StExBar/src/stdafx.h
+++ StExBar/src/stdafx.h
@@ -9,16 +9,16 @@
 #   define WINVER 0x0600            // Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
 #endif
 
-#ifndef _WIN32_WINNT                // Allow use of features specific to Windows NT 4 or later.
-#   define _WIN32_WINNT 0x0600      // Change this to the appropriate value to target Windows 98 and Windows 2000 or later.
+#ifndef _WIN32_WINNT                // Allow use of features specific to Windows Vista or later.
+#   define _WIN32_WINNT 0x0600      // Change this to the appropriate value to target Windows Vista or later.
 #endif
 
-#ifndef _WIN32_WINDOWS              // Allow use of features specific to Windows 98 or later.
-#   define _WIN32_WINDOWS 0x0600    // Change this to the appropriate value to target Windows Me or later.
+#ifndef _WIN32_WINDOWS              // Allow use of features specific to Vista or later.
+#   define _WIN32_WINDOWS 0x0600    // Change this to the appropriate value to target Windows Vista or later.
 #endif
 
-#ifndef _WIN32_IE                   // Allow use of features specific to IE 4.0 or later.
-#   define _WIN32_IE 0x600          // Change this to the appropriate value to target IE 5.0 or later.
+#ifndef _WIN32_IE                   // Allow use of features specific to IE 7.0 or later.
+#   define _WIN32_IE 0x700          // Change this to the appropriate value to target IE 7.0 or later.
 #endif
 
 // we're a dll loaded by the shell: this define is needed to use the version 6 common controls
