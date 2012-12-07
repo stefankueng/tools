faa7f694dcb563062b75070319c8f5705a80939f
XhmikosR <xhmikosr@gmail.com>
2012-12-07 09:42:44 +0000

    SKTimeStamp: move manifestdependency in stdafx.h

--- SKTimeStamp/src/stdafx.h
+++ SKTimeStamp/src/stdafx.h
@@ -12,6 +12,10 @@
 
 #include <SDKDDKVer.h>
 
+
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+
 #define ISOLATION_AWARE_ENABLED 1
 
 
