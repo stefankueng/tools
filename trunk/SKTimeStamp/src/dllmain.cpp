be43d01874a98e131bb921d4b20b9b9bd00d3ff0
XhmikosR <XhmikosR@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2012-12-07 09:42:44 +0000

    SKTimeStamp: move manifestdependency in stdafx.h

--- trunk/SKTimeStamp/src/dllmain.cpp
+++ trunk/SKTimeStamp/src/dllmain.cpp
@@ -7,7 +7,6 @@
 HINSTANCE   g_hmodThisDll = NULL;           ///< handle to this DLL itself.
 UINT        g_cRefThisDll = 0;              ///< reference count of this DLL.
 
-#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 
 extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance,
                                DWORD dwReason,
