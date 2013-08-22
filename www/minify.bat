6164ef380b39aee570ee7f0e884a402b7b185752
XhmikosR <xhmikosr@gmail.com>
2013-08-22 00:53:16 +0000

    Switch to vanilla normalize.css.

--- www/minify.bat
+++ www/minify.bat
@@ -5,7 +5,7 @@ rem npm install -g uglify-js
 
 pushd %~dp0
 
-type css\style.css css\normalize.css css\jquery.fancybox.css | cleancss --s0 -o css\pack.css
+type css\normalize.css css\style.css css\jquery.fancybox.css | cleancss --s0 -o css\pack.css
 cmd /c uglifyjs js\plugins.js js\jquery.fancybox.js js\jquery.mousewheel.js --compress --mangle -o js\pack.js
 
 popd
