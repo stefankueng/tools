c096be597786f51c63612c02dab0c8e2a55a5d25
XhmikosR <xhmikosr@gmail.com>
2014-05-10 15:17:28 +0000

    www: improve UploadWebsite.bat.

--- www/UploadWebsite.bat
+++ www/UploadWebsite.bat
@@ -1,30 +1,39 @@
 @echo off
-rem run this batch file to upload the docs to our sourceforge server
-rem to make this work, you have to create a file "serverlogin.bat"
-rem which sets the variables USERNAME and PASSWORD, and also set the PSCP
-rem variable to point to your scp program the PLINK variable to the plink
-rem program and the ZIP variable to your zip program
+rem Run this batch file to upload the files to the SourceForge server.
+rem To make this work, you have to create a file "serverlogin.bat"
+rem which sets the USERNAME, PASSWORD and PSCP variables
 
-rem example serverlogin.bat file
+rem example serverlogin.bat file:
 rem
 rem @echo off
 rem set USERNAME=myname
 rem set PASSWORD=mypassword
 rem set PSCP="C:\Programme\PuttY\pscp.exe"
-rem set PLINK="C:\Programme\Putty\plink.exe"
-rem set ZIP="C:\Programme\7-zip\7z.exe"
+
 
 pushd %~dp0
 
 if not exist ..\serverlogin.bat echo No login information provided! && goto end
 
-if not exist "node_modules" cmd /c npm install
-cmd /c grunt build
+rem Make sure we have all the deps up to date
+cmd /c npm install
+if %errorlevel% neq 0 (
+  echo `npm install` failed. Please try again later.
+  goto end
+)
 
+rem Run the actual build command
+cmd /c grunt build
+if %errorlevel% neq 0 (
+  echo `grunt build` failed. Check the build log and try again later.
+  goto end
+)
+
+rem Get the login info
 call ..\serverlogin.bat
 
 pushd dist\
-
+rem Upload the files to the server
 %PSCP% -l %USERNAME% -pw %PASSWORD% -r *.* web.sourceforge.net:/home/project-web/stefanstools/htdocs/
 
 popd
