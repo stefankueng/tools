1fb617c16d367a99c88ed88837dae65f383de56e
steveking <steveking@6da1ca88-49ab-40e3-90c0-b0a4d832ced0>
2014-10-27 08:35:27 +0000

    Create a chocolatey build for StExBar.

---
+++ trunk/StExBar/choco/chocolateyInstall.ps1
@@ -0,0 +1,1 @@
+Install-ChocolateyPackage 'StExBar' 'msi' '/quiet' 'http://sourceforge.net/projects/stefanstools/files/StExBar/StExBar-$MajorVersion$.$MinorVersion$.$MicroVersion$.msi/download' 'http://sourceforge.net/projects/stefanstools/files/StExBar/StExBar64-$MajorVersion$.$MinorVersion$.$MicroVersion$.msi/download' 
