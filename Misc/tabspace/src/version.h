6f903bf7cdbb19e5a599d61960ffd2ec24ad1a19
Stefan Küng <665591+stefankueng@users.noreply.github.com>
2017-03-28 17:43:35 +0000

    bump version to 1.1.5

--- Misc/tabspace/src/version.h
+++ Misc/tabspace/src/version.h
@@ -1,4 +1,4 @@
-#define FILEVER        1,1,4,0
-#define PRODUCTVER     1,1,4,0
-#define STRFILEVER     "1.1.4.0\0"
-#define STRPRODUCTVER  "1.1.4.0\0"
+#define FILEVER        1,1,5,0
+#define PRODUCTVER     1,1,5,0
+#define STRFILEVER     "1.1.5.0\0"
+#define STRPRODUCTVER  "1.1.5.0\0"
