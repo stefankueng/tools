e21908c2a87e8c54cef2392c0b76e21b93bace4d
Stefan Küng <stefankueng@gmail.com>
2022-10-11 18:09:05 +0200

    bump tabspace version to 1.1.7

--- Misc/tabspace/src/version.h
+++ Misc/tabspace/src/version.h
@@ -1,4 +1,4 @@
-#define FILEVER        1,1,6,0
-#define PRODUCTVER     1,1,6,0
-#define STRFILEVER     "1.1.6.0\0"
-#define STRPRODUCTVER  "1.1.6.0\0"
+#define FILEVER        1,1,7,0
+#define PRODUCTVER     1,1,7,0
+#define STRFILEVER     "1.1.7.0\0"
+#define STRPRODUCTVER  "1.1.7.0\0"
